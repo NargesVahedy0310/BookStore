47953f432f3094158e45860f58cd25b08279b53f
narges <nargesvahedy0310@gmail.com>
2023-08-14 18:46:55 +0330

    create model book

--- books/models.py
+++ books/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    authors = models.ManyToManyField('Author')
+    genre = models.ForeignKey('Genre', on_delete=models.SET_NULL, null=True)
+    publication_date = models.DateField()
+    isbn = models.CharField(max_length=13)  # شابک کتاب
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.title
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    birth_city = models.ForeignKey('City', on_delete=models.SET_NULL, null=True)
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
