42ecda7a36d18f96f17b7f2bd3d9f7eebfd60a06
narges <nargesvahedy0310@gmail.com>
2023-08-15 14:43:17 +0330

    pagenation  list book

--- books/views.py
+++ books/views.py
@@ -1,7 +1,9 @@
 from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.pagination import PageNumberPagination
 
 class BookListView(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    pagination_class = PageNumberPagination
